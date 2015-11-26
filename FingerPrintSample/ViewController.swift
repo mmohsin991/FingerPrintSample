5f691ca7996f3ca0d3d56e6b81c760055be58323
Mohsin <muhammad.mohsin.991@gmail.com>
2015-11-26 16:38:02 +0500

    Initial Commit

---
+++ FingerPrintSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FingerPrintSample
+//
+//  Created by Mohsin on 26/11/2015.
+//  Copyright © 2015 Mohsin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
